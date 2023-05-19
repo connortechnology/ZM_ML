723a226babd6a144c583c0a87ac8a457d5256ef5
baudneo <86508179+baudneo@users.noreply.github.com>
2023-05-18 21:58:51 -0600

    Better logic and add minimal error handling in EventStartCommand.sh.

--- examples/EventStartCommand.sh
+++ examples/EventStartCommand.sh
@@ -3,8 +3,17 @@ trap 'cleanup' SIGINT SIGTERM
 cleanup() {
   exit 1
 }
-MID=$2
-EID=$1
+if [[ -z "$1" ]]; then
+  echo "An event ID MUST be passed as ARG 1, Monitor ID for ARG 2 is optional"
+  exit 1
+  else
+    EID="--eid $1"
+fi
+if [[ -n "$2" ]]; then
+  MID="--mid $MID"
+  else
+    MID=""
+fi
 
 config="${ML_CLIENT_CONF_FILE:-/etc/zm/client.yml}"
 detect_script="${ML_CLIENT_EVENT_START:-$(which zmml_eventproc)}"
@@ -13,10 +22,10 @@ event_start_command=(
   python3
   "${detect_script}"
   --config "${config}"
-  --eid "${EID}"
-   --mid "${MID}"
-   --live
-   --event-start
+  --live
+  --event-start
+  "${EID}"
+  "${MID}"
 )
 
 eval "${event_start_command[@]}"
