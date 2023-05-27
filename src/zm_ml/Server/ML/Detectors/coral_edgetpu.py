ac676029db0f1038ad3a0e28b0eed7313b34f4f1
baudneo <86508179+baudneo@users.noreply.github.com>
2023-05-26 19:28:51 -0600

    Add NMS. Remove resizing logic as the pycoral lib handles that for us.

--- src/zm_ml/Server/ML/Detectors/coral_edgetpu.py
+++ src/zm_ml/Server/ML/Detectors/coral_edgetpu.py
@@ -67,44 +67,75 @@ class TpuDetector(FileLock):
         else:
             logger.debug(f"perf:{LP} loading took: {time.perf_counter() - t:.5f}s")
 
+    def nms(self, objects, threshold):
+        """Returns a list of indexes of objects passing the NMS.
+
+        Args:
+          objects: result candidates.
+          threshold: the threshold of overlapping IoU to merge the boxes.
+
+        Returns:
+          A list of indexes containing the objects that pass the NMS.
+        """
+        if len(objects) == 1:
+            return [0]
+
+        boxes = np.array([o.bbox for o in objects])
+        xmins = boxes[:, 0]
+        ymins = boxes[:, 1]
+        xmaxs = boxes[:, 2]
+        ymaxs = boxes[:, 3]
+
+        areas = (xmaxs - xmins) * (ymaxs - ymins)
+        scores = [o.score for o in objects]
+        idxs = np.argsort(scores)
+
+        selected_idxs = []
+        while idxs.size != 0:
+            selected_idx = idxs[-1]
+            selected_idxs.append(selected_idx)
+
+            overlapped_xmins = np.maximum(xmins[selected_idx], xmins[idxs[:-1]])
+            overlapped_ymins = np.maximum(ymins[selected_idx], ymins[idxs[:-1]])
+            overlapped_xmaxs = np.minimum(xmaxs[selected_idx], xmaxs[idxs[:-1]])
+            overlapped_ymaxs = np.minimum(ymaxs[selected_idx], ymaxs[idxs[:-1]])
+
+            w = np.maximum(0, overlapped_xmaxs - overlapped_xmins)
+            h = np.maximum(0, overlapped_ymaxs - overlapped_ymins)
+
+            intersections = w * h
+            unions = areas[idxs[:-1]] + areas[selected_idx] - intersections
+            ious = intersections / unions
+
+            idxs = np.delete(
+                idxs, np.concatenate(([len(idxs) - 1], np.where(ious > threshold)[0])))
+
+        return selected_idxs
+
     def detect(self, input_image: np.ndarray):
-        from pycoral.adapters import common as common, detect as detect
+        from pycoral.adapters import common, detect
         b_boxes, labels, confs = [], [], []
         h, w = input_image.shape[:2]
-        _h, _w = self.config.height, self.config.width
         if not self.model:
             logger.warning(f"{LP} model not loaded? loading now...")
             self.load_model()
-        x_factor: float = 1.00
-        y_factor: float = 1.00
         t = time.perf_counter()
-        model_resize = False
-        if _h != h or _w != w:
-            model_resize = True
-            input_image = cv2.resize(
-                input_image, (_w, _h), interpolation=cv2.INTER_AREA
-            )
-            # get scaling so we can make correct bounding boxes
-            x_factor = w / _w
-            y_factor = h / _h
-
         input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
         input_image = Image.fromarray(input_image)
         logger.debug(
-            f"{LP}detect: input image {w}*{h} model input {_w}*{_h}{' (resized)' if model_resize else ''}"
+            f"{LP}detect: input image {w}*{h}"
         )
-
+        # scale = min(orig_width / w, orig_height / h)
         _, scale = common.set_resized_input(
             self.model,
             input_image.size,
             lambda size: input_image.resize(size, Image.ANTIALIAS),
-            # lambda size: input_image.resize(size, Image.LANCZOS),
         )
         try:
             self.acquire_lock()
             self.model.invoke()
         except Exception as ex:
-            logger.error(f"{LP} TPU error: {ex}")
+            logger.error(f"{LP} TPU error while calling invoke(): {ex}")
             raise ex
         else:
             objs = detect.get_objects(self.model, self.options.confidence, scale)
@@ -113,10 +144,12 @@ class TpuDetector(FileLock):
             )
         finally:
             self.release_lock()
-        logger.debug(f"{LP} {len(objs)} objects detected")
+        logger.debug(f"{LP} {len(objs)} objects detected, applying NMS filter...")
+        # Non Max Suppression
+        nms_threshold = self.config.detection_options.nms
+        objs = [objs[i] for i in self.nms(objs, nms_threshold)]
+        logger.debug(f"{LP} {len(nms_objs)} objects after NMS filtering with threshold: {nms_threshold}")
         for obj in objs:
-            if obj.id > len(self.config.labels):
-                logger.warning(f"{LP} {obj.id = } is out of range of the supplied labels, does obj.id start at 1 instead of 0 for calling an array?")
             b_boxes.append(
                 [
                     int(round(obj.bbox.xmin)),
@@ -127,18 +160,6 @@ class TpuDetector(FileLock):
             )
             labels.append(self.config.labels[obj.id])
             confs.append(float(obj.score))
-
-        if model_resize and labels:
-            logger.debug(
-                f"{LP} The image was resized before processing by the 'model width/height', scaling "
-                f"bounding boxes in image by factors of -> x={x_factor:.4} "
-                f"y={y_factor:.4}",
-            )
-            for box in b_boxes:
-                box[0] = round(box[0] * x_factor)
-                box[1] = round(box[1] * y_factor)
-                box[2] = round(box[2] * x_factor)
-                box[3] = round(box[3] * y_factor)
 
         return {
             "success": True if labels else False,
