6d69d11e49c9b15dbd8818c68d04b35e036dbd9f
baudneo <86508179+baudneo@users.noreply.github.com>
2023-05-29 11:28:19 -0600

    Rough in exploratory stuff for annotation filter

--- examples/annotate_events_filt.py
+++ examples/annotate_events_filt.py
@@ -9,3 +9,27 @@ to the script. This is the beggining of the exploratory work on how to integrate
      - This allows for filtering by label/group/zone
  - all: annotate anything it finds in the event
 """
+import logging
+import warnings
+import os
+import sys
+
+LP: str = "filter:annotate:"
+logger = logging.getLogger("AnnotateEvents")
+logger.setLevel(logging.DEBUG)
+formatter: logging.Formatter = logging.Formatter(
+    "%(asctime)s.%(msecs)04d %(name)s[%(process)s] %(levelname)s %(module)s:%(lineno)d -> %(message)s",
+    "%m/%d/%y %H:%M:%S",
+)
+ch = logging.StreamHandler(sys.stdout)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+# zm has logrotation enabled for all files ending in .log in its log folder
+fh = logging.FileHandler("/var/log/zm/annotate_events.log")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+if __name__ == "__main__":
+    logger.info(f"{LP} Starting")
+    logger.info(f"{LP} CLI args: { {k: v for k, v in enumerate(sys.argv)} }")
+
