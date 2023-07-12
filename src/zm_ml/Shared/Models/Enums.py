bdc1603f96f874ddf2bfe1cf7c70d007cfc685ec
baudneo <86508179+baudneo@users.noreply.github.com>
2023-07-12 16:42:10 -0600

    remove str() as f-string already casts to str type.

--- src/zm_ml/Shared/Models/Enums.py
+++ src/zm_ml/Shared/Models/Enums.py
@@ -60,7 +60,7 @@ class ModelProcessor(str, Enum):
     DEFAULT = CPU
 
     def __repr__(self):
-        return f"<{self.__class__.__name__}: {str(self.name)}>"
+        return f"<{self.__class__.__name__}: {self.name}>"
 
     def __str__(self):
         return f"{self.name}"
